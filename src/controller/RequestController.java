8f6c477a857ea95abc2098c708b04a5e5300299e
Malin Albinsson <mea733@telia.com>
2018-11-04 01:58:43 +0100

    added/updated controllers for root, Startview and request

--- src/controller/RequestController.java
+++ src/controller/RequestController.java
@@ -1,5 +1,21 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
 public class RequestController {
 //TODO
+	
+	public void openAddNewRequest(ActionEvent actionEvent) {
+		String location = "view/StartView.fxml";
+		//FXMLLoader loader = new FXMLLoader();
+       // loader.setLocation(Main.class.getResource(location));
+        //AnchorPane view = (AnchorPane) loader.load();
+	}
+	
+	public void openViewRequests(ActionEvent actionevent) {
+		
+	}
 }
---
+++ src/controller/StartViewController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+public class StartViewController {
+	 @FXML
+	 private Button openNewRequestFormBtn;
+	 @FXML
+	 private Button openViewRequestFormBtn;
+	 @FXML
+	 private TextArea startMessageConsole;
+	    
+	 //Initializing the controller class.
+    //This method is automatically called after the fxml file has been loaded.
+    @FXML
+    private void initialize () {
+    	
+    }
+}
