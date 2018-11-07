a057bae1c5553c2f33b5e9f8a89864c187e5aeae
jocke <joakim@joakimolofsson.com>
2018-11-07 21:39:27 +0100

    Updated Model for role and status

---
+++ src/model/Role.java
@@ -0,0 +1,32 @@
+class Role {
+
+    //Fields
+    private int role_id;
+    private String role_description;
+
+    /**
+     * @return Get role_id
+     */
+    public int getRole_id {
+        return id;
+    }
+    /**
+     * @param  Set role_id
+     */
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+    /**
+     * @return  Get role_description, Tenant or Employee
+     */
+    public String getRole_Description() {
+        return role_description;
+    }
+
+    /**
+     * @param  Set role_description, Tenant or Employee
+     */
+    public void setRole_description(String role_description) {
+        this.role_description = role_description;
+    }
+}
