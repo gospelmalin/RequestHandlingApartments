31fe3642daa1abff57569d3e6f9040a77c00bf58
Malin Albinsson <mea733@telia.com>
2018-11-10 02:01:12 +0100

    some work on RequestView controller, request fxml-files

--- src/controller/RequestViewController.java
+++ src/controller/RequestViewController.java
@@ -1,12 +1,52 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import model.Request;
+import repository.RequestRepository;
 
 public class RequestViewController {
+	
+	RequestRepository rr = new RequestRepository();
+	
+	
+	@FXML
+	private TableView<Request> requestTable;
+	
+	
+	//TODO
+	/*
+	@FXML
+    TableColumn<District, String> idDistrict;
+	
+	@FXML
+    TableColumn<District, String> idName;
+	*/ 
+	
+	@FXML
+	private void initialize () {
+		//TODO
+		// mouseclick eventhandler
+	//	btnCreateDistrict.setOnMouseClicked(this::handleOnMouseClicked); //TODO
+
+		//TODO
+		// Match column with property
+		//idDistrict.setCellValueFactory(new PropertyValueFactory<District, String>("id"));
+	//	idName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		// Update table
+		updateTable();
+	}
 	//TODO
 	
 		public void openAddNewRequest(ActionEvent actionEvent) {
@@ -43,7 +83,11 @@ public class RequestViewController {
 	    	System.out.println("openViewRequestsForm? should show selected request and allow editing");
 		}
 		
-		
+		 private void updateTable() {
+		    	
+				ObservableList<Request> list = FXCollections.observableArrayList(rr.getAll());
+				requestTable.setItems((ObservableList<Request>) list);
+		    }
 		
 }
 
